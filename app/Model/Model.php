95dd99417510898b0f9bca10248172ff6326b403
Masha <n3mm@mail.ru>
2025-12-22 19:56:39 +0300

    Исправление мелких ошибок

--- app/Model/Model.php
+++ app/Model/Model.php
@@ -1,13 +1,11 @@
 <?php
 
 namespace Model;
-
 use Core\Database;
 
 abstract class Model
 {
     protected $conn;
-
     public function __construct()
     {
         // Инициализируем соединение в базовом классе
